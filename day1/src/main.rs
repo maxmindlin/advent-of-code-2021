43120d656ce2615694fad1787384479018872913
Max Mindlin <maxmindlin@gmail.com>
2021-12-01 18:00:18 -0800

    day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    let f = std::fs::read_to_string("input/1.txt").unwrap();
+    let input = parse_input(&f);
+    println!("ANSWER 1: {}", count(input.clone()));
+    println!("ANSWER 2: {}", count_windows(input));
+}
+
+fn parse_input(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .map(|l| l.trim())
+        .filter(|w| !w.is_empty())
+        .map(|w| w.parse().unwrap())
+        .collect()
+}
+
+fn count(input: Vec<usize>) -> usize {
+    let mut curr = input[0];
+    let mut count = 0;
+    for i in 1..input.len() {
+        let next = input[i];
+        if next > curr {
+            count += 1;
+        }
+        curr = next;
+    }
+
+    count
+}
+
+fn count_windows(input: Vec<usize>) -> usize {
+    let mut curr = std::usize::MAX;
+    let mut count = 0;
+    for w in input.windows(3) {
+        let next = w.iter().sum();
+        if next > curr {
+            count += 1;
+        }
+        curr = next;
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn given() {
+        let input = vec![
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263,
+        ];
+        assert_eq!(count(input), 7);
+    }
+
+    #[test]
+    fn given_windows() {
+        let input = vec![
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263,
+        ];
+        assert_eq!(count_windows(input), 5);
+    }
+}
---
+++ new.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cargo new --bin day$1 && cd day$1 && mkdir input && touch input/1.txt && touch README.md
